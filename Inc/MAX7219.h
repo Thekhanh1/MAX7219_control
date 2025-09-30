d2894f901db6d916ef46d39b81eefcace6bcc665
The Anh <theanh221006@gmail.com>
2025-09-30 15:59:28 +0700

    added support for cascading up to 8 MAX7219/7221

--- Inc/MAX7219.h
+++ Inc/MAX7219.h
@@ -8,7 +8,16 @@
 #ifndef INC_MAX7219_H_
 #define INC_MAX7219_H_
 
+//change to your MCU
 #include "stm32f4xx_hal.h"
+
+// macro to decluter the code
+#define VALIDATE_PARAMS(ld, addr) \
+    do { \
+        if (ld == NULL) return; \
+        if (addr < 0 || addr >= ld->maxDevices) return; \
+    } while(0)
+
 
 // register address
 
@@ -27,6 +36,9 @@
 #define MAX7219_REG_SHUTDOWN        0x0C
 #define MAX7219_REG_DISPLAYTEST     0x0F
 
+
+
+
 // MAX7219 struct for easy handling
 
 typedef struct
@@ -44,11 +56,13 @@ typedef struct
 	// chip select line
 	GPIO_t* CS_line;
 	// data array
-	uint8_t spiData[2];
+	uint8_t spiData[16];
 	// buffer
-	uint8_t status[8];
-	// auto use refresh()
+	uint8_t status[64];
+	// auto use refresh() is enabled by default
 	uint8_t autoRefresh;
+	// maximum number of devices
+	uint8_t maxDevices;
 
 } MAX7219;
 
@@ -56,36 +70,39 @@ typedef struct
  * initialise the MAX7219
  * param:
  * 		ld: pointer to struct
- * 		spi: spi struct from stm32
+ * 		hspi1: SPI struct from stm32 HAL library
+ *		CS_pin: create a GPIO_t struct and pass it in
+ *		num_devices: how many devices in the chain
  *
  */
 
-void MAX7219_init(MAX7219* ld);
+void MAX7219_init(MAX7219* ld, int num_devices, SPI_HandleTypeDef* hspi1, GPIO_t* CS_pin);
 
 /*
  * helper function to communicate with MAX7219
  * param:
  * 		ld: pointer to struct
- * 		spi: spi struct from stm32
- *
+ *		addr: address of the IC (0-7) (0-7)
  */
 
-void SPI_write(MAX7219* ld, uint8_t opcode, uint8_t data);
+void MAX7219_transmit(MAX7219* ld, int addr, uint8_t opcode, uint8_t data);
 
 /*
  * shutdown MAX7219
  * param:
  * 		ld: pointer to struct
+ * 		addr: address of the IC (0-7)
  * 		mode: 0x01 for normal
  * 			  0x00 for shutdown
  */
 
-void shutdown(MAX7219* ld, uint8_t mode);
+void shutdown(MAX7219* ld, int addr, uint8_t mode);
 
 /*
  * set the decode mode
  * param:
  * 		ld: pointer to struct
+ * 		addr: address of the IC (0-7)
  * 		mode: 0x00 for no decode
  * 			  0x01 for code B for 0 only
  * 			  0x0F for code B for 3-0 only
@@ -95,85 +112,91 @@ void shutdown(MAX7219* ld, uint8_t mode);
  */
 
 
-void set_decode(MAX7219* ld, uint8_t mode);
+void set_decode(MAX7219* ld, int addr, int mode);
 
 /*
  * Set the brightness of the display
  * param:
  * 		ld: pointer to struct
+ * 		addr: address of the IC (0-7)
  * 		intensity: 0x00 - 0x0F
  */
 
-void set_intensity(MAX7219* ld, uint8_t intensity);
+void set_intensity(MAX7219* ld, int addr, int intensity);
 
 /*
  * Set the number of digits (or rows) to be displayed
  * param:
  * 		ld: pointer to struct
+ * 		addr: address of the IC (0-7)
  * 		limit: 0x00 - 0x07
  */
 
-void set_scanLimit(MAX7219* ld, uint8_t lim);
+void set_scanLimit(MAX7219* ld, int addr, int lim);
 
 /*
 * turn all leds on
 * param:
 * 		ld: pointer to struct
+* 		addr: address of the IC (0-7)
 * 		mode: 0 for normal
 * 			  1 for all leds on
 */
 
-void set_displayTest(MAX7219* ld, uint8_t mode);
+void set_displayTest(MAX7219* ld, int addr, uint8_t mode);
 
 /*
 * clear the buffer
 * param:
 * 		ld: pointer to struct
-* 		state: 0 for no
-* 			   1 for yes
+* 		addr: address of the IC (0-7)
 */
 
-void clear(MAX7219* ld);
+void clear(MAX7219* ld, int addr);
 
 /*
 * set a single led status
 * param:
 * 		ld: pointer to struct
+* 		addr: address of the IC (0-7)
 * 		row: row of the led
 * 		col: column of the led
 * 		state: 0 for off
 * 			   1 for on
 */
 
-void set_led(MAX7219* ld, uint8_t row, uint8_t col, uint8_t state);
+void set_led(MAX7219* ld, int addr, int row, int col, uint8_t state);
 
 /*
 * set row to be a pattern
 * param:
 * 		ld: pointer to struct
+* 		addr: address of the IC (0-7)
 * 		row: row of the led
 * 		state: 8 bit numer
 */
 
-void set_row(MAX7219* ld, uint8_t row, uint8_t state);
+void set_row(MAX7219* ld, int addr, int row, uint8_t state);
 
 /*
 * set column to be a pattern
 * param:
 * 		ld: pointer to struct
+* 		addr: address of the IC (0-7)
 * 		col: column of the led
 * 		state: 8 bit number
 */
 
-void set_col(MAX7219* ld, uint8_t col, uint8_t state);
+void set_col(MAX7219* ld, int addr, int col, uint8_t state);
 
 /*
 * refresh the status[8] buffer
 * param:
 * 		ld: pointer to struct
+* 		addr: address of the IC (0-7)
 */
 
-void refresh(MAX7219* ld);
+void refresh(MAX7219* ld, int addr);
 
 
 #endif /* INC_MAX7219_H_ */
--- Src/MAX7219.c
+++ Src/MAX7219.c
@@ -7,75 +7,105 @@
 
 #include "MAX7219.h"
 
-void MAX7219_init(MAX7219* ld)
+void MAX7219_init(MAX7219* ld, int num_devices, SPI_HandleTypeDef* hspi1, GPIO_t* CS_pin)
 {
+	if (ld == NULL || hspi1 == NULL || CS_pin == NULL) return;
+	if (num_devices <= 0 || num_devices > 8) num_devices = 8;
+	ld->maxDevices = num_devices;
+	//set up the SPI and the chip select
+    ld->spiHandle = hspi1;
+	ld->CS_line = CS_pin;
+
 	// pull the chip select to idle high
 	HAL_GPIO_WritePin((ld->CS_line)->prt, (ld->CS_line)->pin, 1);
-	ld->spiData[0] = 0;
-	ld->spiData[1] = 0;
-	// clear the buffer
-	for (int i = 0; i < 8; i++)
+
+	// clear the buffer and data in the SPI array
+	for (int i = 0; i < 16; i++)
+		ld->spiData[i] = 0x00;
+	for (int i = 0; i < 64; i++)
 		ld->status[i] = 0x00;
 
-	// no display test
-	set_displayTest(ld, 0);
-	// no decode
-	set_decode(ld, 0x00);
-	// minimum brightness
-	set_intensity(ld, 0x00);
-	// maximum scan-limit
-	set_scanLimit(ld, 0x07);
-	// enter shutdown mode on startup
-	shutdown(ld, 0);
+	for (int i = 0; i < num_devices; i++)
+	{
+		// no display test
+		set_displayTest(ld, i, 0);
+		// no decode
+		set_decode(ld, i, 0x00);
+		// minimum brightness
+		set_intensity(ld, i, 0x00);
+		// maximum scan-limit
+		set_scanLimit(ld, i, 0x07);
+		// wake up the MAX7219 on init
+		shutdown(ld, i, 1);
+	}
+	// default to always refresh
+	ld->autoRefresh = 1;
 };
 
 
-void shutdown(MAX7219* ld, uint8_t mode)
+void shutdown(MAX7219* ld, int addr, uint8_t mode)
 {
-	SPI_write(ld, MAX7219_REG_SHUTDOWN, mode);
+	VALIDATE_PARAMS(ld, addr);
+	// normalisation to 0 or 1
+	mode = (mode != 0x00);
+	MAX7219_transmit(ld, addr, MAX7219_REG_SHUTDOWN, mode);
 };
 
 
-void set_decode(MAX7219* ld, uint8_t mode)
+void set_decode(MAX7219* ld, int addr, int mode)
 {
-	SPI_write(ld, MAX7219_REG_DECODE, mode);
+	VALIDATE_PARAMS(ld, addr);
+	if (mode != 0x00 && mode != 0x01 && mode != 0x0F && mode != 0xFF) return;
+	MAX7219_transmit(ld, addr, MAX7219_REG_DECODE, mode);
 };
 
 
-void set_intensity(MAX7219* ld, uint8_t intensity)
+void set_intensity(MAX7219* ld, int addr, int intensity)
 {
-	SPI_write(ld, MAX7219_REG_INTENSITY, intensity);
+	VALIDATE_PARAMS(ld, addr);
+	if (intensity > 0x0F || intensity < 0x00) return;
+	MAX7219_transmit(ld, addr, MAX7219_REG_INTENSITY, intensity);
 };
 
 
-void set_scanLimit(MAX7219* ld, uint8_t lim)
+void set_scanLimit(MAX7219* ld, int addr, int lim)
 {
-	SPI_write(ld, MAX7219_REG_SCANLIMIT, lim);
+	VALIDATE_PARAMS(ld, addr);
+	if (lim > 0x07 || lim < 0x00) return;
+	MAX7219_transmit(ld, addr, MAX7219_REG_SCANLIMIT, lim);
 };
 
 
-void set_displayTest(MAX7219* ld, uint8_t mode)
+void set_displayTest(MAX7219* ld, int addr, uint8_t mode)
 {
-	SPI_write(ld, MAX7219_REG_DISPLAYTEST, mode);
+	VALIDATE_PARAMS(ld, addr);
+	// normalisation to 0 or 1
+	mode = (mode != 0x00);
+	MAX7219_transmit(ld, addr, MAX7219_REG_DISPLAYTEST, mode);
 };
 
 
-void clear(MAX7219* ld)
+void clear(MAX7219* ld, int addr)
 {
+	VALIDATE_PARAMS(ld, addr);
+
+	uint8_t offset = 8*addr;
 
 	for (uint8_t i = 0; i < 8; i++)
 	{
-		ld->status[i] = 0x00;
+		ld->status[offset + i] = 0x00;
 		if (ld->autoRefresh)
-			SPI_write(ld, i + 1, 0x00);
+			MAX7219_transmit(ld, addr, i + 1, 0x00);
 	}
 };
 
 
-void set_led(MAX7219* ld, uint8_t row, uint8_t col, uint8_t state)
+void set_led(MAX7219* ld, int addr, int row, int col, uint8_t state)
 {
-	if (col > 7 || row > 7)
-		return;
+	VALIDATE_PARAMS(ld, addr);
+	if (col > 7 || row > 7 || col < 0 || row < 0) return;
+
+	uint8_t offset = 8*addr;
 
 	// bit mask to select the right column
 	uint8_t mask = 0x80 >> col;
@@ -83,63 +113,86 @@ void set_led(MAX7219* ld, uint8_t row, uint8_t col, uint8_t state)
 	if (state)
 	{
 		// make the led at the correct row and column be 1
-		ld->status[row] |= mask;
+		ld->status[offset + row] |= mask;
 	} else
 	{
 		// make the led at the correct row and column be 0
-		ld->status[row] &= ~mask;
+		ld->status[offset + row] &= ~mask;
 	}
 
 	if (ld->autoRefresh)
-		SPI_write(ld, row + 1, ld->status[row]);
+		MAX7219_transmit(ld, addr, row + 1, ld->status[offset + row]);
 };
 
 
-void set_row(MAX7219* ld, uint8_t row, uint8_t state)
+void set_row(MAX7219* ld, int addr, int row, uint8_t state)
 {
-	if (row > 7) return;
+	VALIDATE_PARAMS(ld, addr);
+	if (row > 7 || row < 0) return;
 
-	ld->status[row] = state;
+	uint8_t offset = 8*addr;
+
+	ld->status[offset + row] = state;
 
 	if (ld->autoRefresh)
-		SPI_write(ld, row + 1, ld->status[row]);
+		MAX7219_transmit(ld, addr, row + 1, ld->status[offset + row]);
 };
 
 
-void set_col(MAX7219* ld, uint8_t col, uint8_t state)
+void set_col(MAX7219* ld, int addr, int col, uint8_t state)
 {
-	if (col > 7) return;
+	VALIDATE_PARAMS(ld, addr);
+	if (col > 7 || col < 0) return;
+
+	uint8_t offset = 8*addr;
+	// bit mask to select the right column
+	uint8_t mask = 0x80 >> col;
 
 	for (int i = 0; i < 8; i++)
 	{
 		uint8_t tmp = state >> (7 - i);
 		tmp &= 0x01;
-		set_led(ld, i, col, tmp);
+		if (tmp)
+			// make the led at the column be 1
+			ld->status[offset + i] |= mask;
+		else
+			// make the led at the column be 0
+			ld->status[offset + i] &= ~mask;
 	}
+
+	if (ld->autoRefresh)
+		refresh(ld, addr);
 };
 
 
-void SPI_write(MAX7219* ld, uint8_t opcode, uint8_t data)
+void MAX7219_transmit(MAX7219* ld, int addr, uint8_t opcode, uint8_t data)
 {
+	VALIDATE_PARAMS(ld, addr);
+
+	uint8_t maxData = ld->maxDevices*2;
+	// sending data is reversed, the first device will receive its data last and vice versa
+	uint8_t reverse_offset = maxData - 2*addr - 2;
+	// clear the data so the only device we want to talk to receives data, all the other ones will receive NO_OP instead
+	for (int i = 0; i < maxData; i++)
+		ld->spiData[i] = 0x00;
 	// opcode first, data second
-	ld->spiData[0] = opcode;
-	ld->spiData[1] = data;
+	ld->spiData[reverse_offset] = opcode;
+	ld->spiData[reverse_offset + 1] = data;
 
 	// pull CS low
 	HAL_GPIO_WritePin((ld->CS_line)->prt, (ld->CS_line)->pin, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(ld->spiHandle, ld->spiData, 2, 100);
+	HAL_SPI_Transmit(ld->spiHandle, ld->spiData, maxData, 100);
 	// latches data
 	HAL_GPIO_WritePin((ld->CS_line)->prt, (ld->CS_line)->pin, GPIO_PIN_SET);
 
 };
 
-void refresh(MAX7219* ld)
+void refresh(MAX7219* ld, int addr)
 {
-	// flush the buffer all at once
+	VALIDATE_PARAMS(ld, addr);
+
+	uint8_t offset = 8*addr;
+	// flush the buffer of a device all at once
 	for (int i = 0; i < 8; i++)
-		SPI_write(ld, i + 1, ld->status[i]);
-}
-
-
-
-
+		MAX7219_transmit(ld, addr, i + 1, ld->status[offset + i]);
+};
